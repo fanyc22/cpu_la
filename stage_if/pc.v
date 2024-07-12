029566ba19503574ed0aaec64802a5950a4e4da5
fanyc22 <fanyc22@mails.tsinghua.edu.cn>
2024-07-12 16:39:48 +0800

    feat: :hammer: add pc.v
    add pc.v

---
+++ stage_if/pc.v
@@ -0,0 +1,53 @@
+module pc(/*autoport*/
+//output
+      pc_reg,
+//input
+      rst_n,
+      clk,
+      enable,
+      branch_address,
+      is_branch,
+      //is_exception,
+      //exception_new_pc,
+    );
+
+parameter PC_INITIAL = 32'h0000_0000;
+
+input wire rst_n;
+input wire clk;
+input wire enable;
+
+input wire[31:0] branch_address;
+input wire is_branch;
+//input wire is_exception;
+//input wire[31:0] exception_new_pc;
+
+reg[31:0] pc_next;
+output reg[31:0] pc_reg;
+
+always @(*) begin
+    if (!rst_n) begin
+        pc_next <= PC_INITIAL;
+    end
+    else if(enable) begin
+        if(is_branch) begin
+            pc_next <= branch_address;
+        end
+        // else if(is_exception) begin
+        //     pc_next <= exception_new_pc;
+        // end
+        else begin
+            pc_next <= pc_reg+32'd4;
+        end
+    end 
+    else begin 
+        pc_next <= pc_reg;
+    end
+end
+
+always @(posedge clk) 
+    pc_reg <= pc_next;
+
+// always @(posedge clk) $display("PC=%x",pc_reg);
+
+endmodule
